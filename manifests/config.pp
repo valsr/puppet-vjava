f059cb5b8e1facdf10fdcb6b9d943902a0453260
valsr <valsr@valsr.com>
2018-01-04 19:08:11 -0500

    Inherit params instead of include

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,9 +1,7 @@
 # Configures java installation. Currently this sets up default java version
 #
 # Copyright 2017 valsr
-class vjava::config{
-  include vjava::globals
-
+class vjava::config inherits vjava::params{
   $java_alternative=$vjava::params::java[$vjava::params::default_java_version][jre][alternative]
   $java_alternative_path=$vjava::params::java[$vjava::params::default_java_version][jre][alternative_path]
   $java_home=$vjava::params::java[$vjava::params::default_java_version][jre][java_home]
