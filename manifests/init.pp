743f12390738dbef6271b5971ddcd8d24f22fe0b
valsr <valsr@valsr.com>
2018-10-26 18:44:33 -0400

    refractored main class (use hiera instead of helper config class)

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,16 +6,44 @@
 #   include vjava
 
 # Copyright 2017 valsr
-class vjava(Optional[Integer] $default_version){
+class vjava(
+  Optional[Integer] $default_version,
+  Optional[String] $alternative,
+  Optional[String] $alternative_path,
+  Optional[String] $java_home,
+  ){
   if $default_version {
-    include "vjava::java_${default_version}::jre"
-    include vjava::config
+    contain "vjava::java_${default_version}::jre"
 
-    Class["vjava::java_${default_version}::jre"]->Class['vjava::config']
+    case $facts['os']['family'] {
+      'Debian': {
+        if $alternative != undef and $alternative_path != undef {
+          exec { 'update-java-alternatives':
+            path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+            command => "update-java-alternatives --set ${alternative}",
+            unless  => "test /etc/alternatives/java -ef '${alternative_path}'",
+            require => Package['java-common']
+          }
+        }
+        elsif $java_home != undef {
+          file_line { 'java-home-environment':
+            path  => '/etc/environment',
+            line  => "JAVA_HOME=${$java_home}",
+            match => 'JAVA_HOME=',
+          }
+        }
+      }
+      default: {
+        # Do nothing.
+      }
+    }
   }
 
-  if $::osfamily == 'Debian' {
-    # Needed for update-java-alternatives
-    v_ensure_packages('java-common')
-  }
+  case $facts['os']['family'] {
+    'Debian': {
+        v_ensure_packages('java-common')
+      }
+      default: {}
+    }
+
 }
